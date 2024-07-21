e4a76184c4005ac6076ebeff3b2ff007fe3725bc
darren <chensanlong@guixiaotu.com>
2024-07-21 18:41:24 +0800

    update

--- mi.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    filename = "/Users/xo9/Downloads/MiFitness data/20240705_1442533652_MiFitness_hlth_center_sport_record.csv"
-
-    df = pd.read_csv(filename)
-    print(df)
